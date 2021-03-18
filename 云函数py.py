e566038ee34587c4743d40ee759d032de1851dcf
walt <walt1012@foxmail.com>
2021-03-19 01:03:02 +0800

    Update 云函数py.py

--- 云函数py.py
+++ 云函数py.py
@@ -1,10 +1,10 @@
 import requests ,os
 # server酱开关，填0不开启(默认)，填2同时开启cookie失效通知和签到成功通知
-sever = 'on'
+sever = 'off'
 # 填写server酱sckey,不开启server酱则不用填（自己更改）
 sckey = 'SCU89402Tf98b7f01ca3394b9ce9aa5e2ed1a****************'
 # 填入glados账号对应cookie
-cookie = '__cfduid=d825014ac33b402b3f**************'
+cookie = '__cfduid=deb58b9412593d6c3f36d06d91a1af19d1616083430'
 referer = 'https://glados.rocks/console/checkin'
 
 def start():
